ec7f23fc3dc09ff7d56b2dee098a92abf5166eaa
André Canuto <andrecanutoc@gmail.com>
2022-03-17 18:27:50 -0300

    feat:theme cvc colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
     purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: '#f8db1c',
+                secondary: '#263c70'
+            }
+        },
     },
     variants: {
         extend: {},
