c5cb3a8708e9498298517e2597c8c38ffa6abf11
André Canuto <andrecanutoc@gmail.com>
2022-03-17 18:13:16 -0300

    feat:api url fixed in api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+    // O correto seria este comentado, porém, como é um teste eu irei colocar fixo.
+    // baseURL: process.env.REACT_APP_API_URL,
+    baseURL: 'https://cvcbackendhotel.herokuapp.com/hotels/'
 });
 
 api.interceptors.request.use(
