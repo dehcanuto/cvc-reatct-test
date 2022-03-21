4647f5346a5f3cb0fd29695f9932518958e3a328
André Canuto <andrecanutoc@gmail.com>
2022-03-21 11:31:01 -0300

    fix: load

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,22 +13,10 @@ const HomePage: React.FC = () => {
     const [filters, setFilters] = useState<number[]>([]);
     const [load, setLoad] = useState<boolean>(true);
 
-    // Funciona apenas para disparar quando o `cityID` muda.
-    useEffect(() => {
-        setLoad(true);
-        setCityName(cities?.find(city => city.cityCode === Number(cityID))?.cityName);
-        const init = async () => {
-            let results = await getAllData(cityID);
-            setHotels(await results);
-            setHotelsFilter(await results);
-        }
-        init()
-            .then(() => {setLoad(false)})
-            .catch(console.error);
-    }, [cityID]);
-
     // Separei para não interferir nas buscas na api e usar apenas dados já guardados no estado.
     useEffect(() => {
+        console.log('!hotels', !hotels);
+        if (!hotels.length) return;
         setLoad(true);
         const filter = async () => {
             let result = hotels;
@@ -43,9 +31,24 @@ const HomePage: React.FC = () => {
             );
         }
         filter()
-            .then(() => {setLoad(false)})
+            .then(() => {setLoad(false); console.log('load filter', load);})
             .catch(console.error);
     }, [filters]);
+
+    // Funciona apenas para disparar quando o `cityID` muda.
+    useEffect(() => {
+        setLoad(true);
+        setCityName(cities?.find(city => city.cityCode === Number(cityID))?.cityName);
+        const init = async () => {
+            setHotelsFilter([]);
+            let results = await getAllData(cityID);
+            setHotels(await results);
+            setHotelsFilter(await results);
+        }
+        init()
+            .then(() => {setLoad(false); console.log('load init', load);})
+            .catch(console.error);
+    }, [cityID]);
 
     console.log('load', load);
 
@@ -55,7 +58,7 @@ const HomePage: React.FC = () => {
                 getCity={(e: number) => setCityID(e)}
                 getLimit={(limit: number[]) => setFilters(limit)}
             />
-            <section className="container mx-auto pt-14">
+            <section className="container mx-auto pt-14 pb-5">
                 <h2 className="text-center text-xl sm:text-left sm:text-3xl font-bold my-8">
                     Resultados para:
                     <span className="text-blue-400 ml-3">
@@ -63,7 +66,7 @@ const HomePage: React.FC = () => {
                     </span>
                 </h2>
                 <div className="grid grid-cols-1 sm:grid-cols-4 gap-4 items-center justify-center">
-                    { load ? <LoadCard /> : !hotelsFilter.length && <p className="text-lg text-gray-400">Sem resultados.</p> }
+                    { load ? <LoadCard /> : !hotelsFilter.length && <p className="text-lg text-center sm:text-left text-gray-400">Sem resultados.</p> }
                     { hotelsFilter && hotelsFilter?.map((item: Hotel, index) => <Card key={index} data={item}/>) }
                 </div>
             </section>
